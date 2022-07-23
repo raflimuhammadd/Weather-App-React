adb323b4ebcf4c9781427b946bde467a7605b49c
raflimuhammadd <raflimuhh09@gmail.com>
2022-07-23 13:16:44 +0700

    ADD-All

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "271628941emshba5ed4191f1e496p1787acjsnbb0d50a62f3b",// enter your rapid api key here
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "43571e33c83f9e78f86dab6af11ba5ca";
