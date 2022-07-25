6b9d9974a10b4371bfda0c913fca3219bf031a44
raflimuhammadd <83290647+raflimuhammadd@users.noreply.github.com>
2022-07-25 22:32:43 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -9,4 +9,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "43571e33c83f9e78f86dab6af11ba5ca";
+export const WEATHER_API_KEY = "ENTER UR KEY";
